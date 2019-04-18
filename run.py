e5bab43d952cb9be8ddd50cbdbd9672e7a5703f2
Silviu <silviumuraru90@gmail.com>
2019-04-18 16:06:39 +0300

    Workaround for the 500 error

--- run.py
+++ run.py
@@ -39,4 +39,4 @@ payload = {
 "store_id": result_id
 }
 
-requests.post("https://ecnaoptriha.herokuapp.com/item/{}".format(id_generator()), data=json.dumps(payload), headers=headers)
+# requests.post("https://ecnaoptriha.herokuapp.com/item/{}".format(id_generator()), data=json.dumps(payload), headers=headers)
