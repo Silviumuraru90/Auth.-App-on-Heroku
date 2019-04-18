c7e99441b52ea007bfa106615c12dc607fecedd4
Silviu <silviumuraru90@gmail.com>
2019-04-18 15:39:42 +0300

    FrontPage modified according to the app's items

--- run.py
+++ run.py
@@ -1,6 +1,7 @@
 from app import app
 from db import db
 from flask import Flask, render_template, request
+import requests, random, string, json
 
 db.init_app(app)
 
@@ -14,10 +15,25 @@ def create_tables():
 def student():
    return render_template('home.html')
 
+
+def id_generator(size=10, chars=string.ascii_uppercase + string.digits):
+    return ''.join(random.choice(chars) for _ in range(size))
+
+headers = {"Content-Type": "application/json"}
+
 @app.route('/result',methods = ['POST', 'GET'])
 def result():
 	if request.method == 'POST':
 		result = request.form
-		print(result['Item'])
 		print(result['Price'])
+		print(result['Id'])
+
+
+        payload = {
+        "price": result['Price'],
+        "store_id": result['Id']
+              }
+        r = requests.post("https://ecnaoptriha.herokuapp.com/item/{}".format(id_generator()), headers=headers, data=json.dumps(payload))
+
+
 		return render_template("result.html",result = result)
