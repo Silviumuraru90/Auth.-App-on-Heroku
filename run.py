0bbea0362619db48f552bf39b4152d20642a4bde
Silviu <silviumuraru90@gmail.com>
2019-04-18 14:59:07 +0300

    Retrieving form data (working)

--- run.py
+++ run.py
@@ -1,7 +1,6 @@
 from app import app
 from db import db
-from flask import Flask, render_template
-import requests
+from flask import Flask, render_template, request
 
 db.init_app(app)
 
@@ -10,47 +9,15 @@ db.init_app(app)
 def create_tables():
     db.create_all()
 
+
 @app.route('/')
-def home():
-    return render_template("home.html")
+def student():
+   return render_template('home.html')
 
-
-
-
-numCarsEast = None
-numCarsWest = None
-numCarsSouth = None
-numCarsNorth = None
-
-
-@app.route('/info.json', methods=['GET', 'POST'])
-def getInfo():
-    lightEast = {}
-    lightWest = {}
-    lightNorth = {}
-    lightSouth = {}
-    intersection1 = {}
-    lightEast['cars'] = numCarsEast
-    lightWest['cars'] = numCarsWest
-    lightNorth['cars'] = numCarsNorth
-    lightSouth['cars'] = numCarsSouth
-    intersection1['eastLight'] = lightEast
-    intersection1['westLight'] = lightWest
-    intersection1['northLight'] = lightNorth
-    intersection1['southLight'] = lightSouth
-    return jsonify(intersection=intersection1)
-
-
-@app.route('/cars', methods=['GET', 'POST'])
-def cars():
-    global numCarsEast, numCarsWest, numCarsSouth, numCarsNorth
-    numCarsEast = requests.form.get('eastLightInt1', None)
-    numCarsWest = requests.form.get('westLightInt1', None)
-    numCarsNorth = requests.form.get('northLightInt1', None)
-    numCarsSouth = requests.form.get('southLightInt1', None)
-    print(str(numCarsEast) + ' east')
-    print(str(numCarsWest) + ' west')
-    print(str(numCarsNorth) + ' north')
-    print(str(numCarsSouth) + ' south')
-    return 'done'
-    return open('./carForm.html').read()
+@app.route('/result',methods = ['POST', 'GET'])
+def result():
+	if request.method == 'POST':
+		result = request.form
+		print(result['Item'])
+		print(result['Price'])
+		return render_template("result.html",result = result)
