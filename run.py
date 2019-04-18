2f36e9c7a05c5d2db6f879fd2fff4ad9343c0f59
Silviu <silviumuraru90@gmail.com>
2019-04-18 17:43:27 +0300

    POST API and Redirect to result.html

--- run.py
+++ run.py
@@ -26,7 +26,7 @@ def id_generator(size=10, chars=string.ascii_uppercase + string.digits):
 def result():
     if request.method == 'POST':
         result = request.form
-        # requests.post("https://ecnaoptriha.herokuapp.com/item/{}".format(id_generator()), data=json.dumps({"price":15.99, "store_id":1}), headers={"Content-Type": "application/json"})
+        requests.post("https://ecnaoptriha.herokuapp.com/item/{}".format(id_generator()), data=json.dumps({"price":15.99, "store_id":1}), headers={"Content-Type": "application/json"})
         return render_template("result.html",result = result)
 
 # payload = {
