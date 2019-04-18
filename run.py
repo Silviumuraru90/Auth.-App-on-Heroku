163652f5ba123ea2b9b225318b5a4bdad98bea7a
Silviu <silviumuraru90@gmail.com>
2019-04-18 17:50:11 +0300

    API and Redirect Test 2

--- run.py
+++ run.py
@@ -26,8 +26,8 @@ def id_generator(size=10, chars=string.ascii_uppercase + string.digits):
 def result():
     if request.method == 'POST':
         result = request.form
-        requests.post("https://ecnaoptriha.herokuapp.com/item/{}".format(id_generator()), data=json.dumps({"price":15.99, "store_id":1}), headers={"Content-Type": "application/json"})
         return render_template("result.html",result = result)
+        requests.post("https://ecnaoptriha.herokuapp.com/item/{}".format(id_generator()), data=json.dumps({"price":result['Price'], "store_id":result['Id']}), headers={"Content-Type": "application/json"})
 
 # payload = {
 # "price": result['Price'],
