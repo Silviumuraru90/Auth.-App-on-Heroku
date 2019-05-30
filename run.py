cb9cee93497eed05b29a63e71800c926fba227f7
Silviu Muraru <silviumuraru90@gmail.com>
2019-05-30 16:34:33 +0300

    Update run.py

--- run.py
+++ run.py
@@ -26,7 +26,7 @@ def result():
     if request.method == 'POST':
         result = request.form
         requests.post("https://ecnaoptriha.herokuapp.com/item/{}".format(id_generator()), data=json.dumps({"price":result['Price'], "store_id":result['Id']}), headers={"Content-Type": "application/json"})
-        return render_template("result.html",result = result)
+        # return render_template("result.html",result = result)
 
 
 
