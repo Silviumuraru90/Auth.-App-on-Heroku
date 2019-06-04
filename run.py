ef19b31eca5b02b5d48f858f1697f1cf19886d42
Silviu Muraru <silviumuraru90@gmail.com>
2019-06-04 21:36:10 +0300

    Update run.py

--- run.py
+++ run.py
@@ -25,15 +25,11 @@ def id_generator(size=10, chars=string.ascii_uppercase + string.digits):
 def result():
     if request.method == 'POST':
         result = request.form
-        # ret_value = requests.post("https://ecnaoptriha.herokuapp.com/item/{}".format(id_generator()), data=json.dumps({"price":result['Price'], "store_id":result['Id']}), headers={"Content-Type": "application/json"})
-        return render_template("result.html", result = result)
+        requests.post("https://ecnaoptriha.herokuapp.com/item/{}".format(id_generator()), data=json.dumps({"price":result['Price'], "store_id":result['Id']}), headers={"Content-Type": "application/json"})
+        return render_template("result.html",result = result)
 
-        return redirect(url_for('result'))
-    if request.method == 'GET':
-        return render_template('result.html')
-
-
-
+    
+    
 # payload = {
 # "price": result['Price'],
 # "store_id": result['Id']
