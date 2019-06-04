2207a25bc0fb0cf10008df1883ccb2ad45c3b120
Silviu Muraru <silviumuraru90@gmail.com>
2019-06-04 22:34:45 +0300

    Update run.py

--- run.py
+++ run.py
@@ -24,15 +24,15 @@ def id_generator(size=10, chars=string.ascii_uppercase + string.digits):
 def functie(x):
     requests.post("https://ecnaoptriha.herokuapp.com/item/{}".format(id_generator()), data=json.dumps({"price":x['Price'], "store_id":x['Id']}), headers={"Content-Type": "application/json"})
    
-a = dict()
+# a = dict()
 
 @app.route('/result', methods = ['POST', 'GET'])
 def result():
     if request.method == 'POST':
         result = request.form
         functie(result)
-        global a = result
-    return render_template("result.html",result = a)
+        # global a = result
+    return render_template("result.html",result = result)
 
     
     
