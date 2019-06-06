46c23e52a60261396755ebf099eb019d2690ef9b
Silviu Muraru <silviumuraru90@gmail.com>
2019-06-06 23:04:26 +0300

    Update run.py

--- run.py
+++ run.py
@@ -31,7 +31,7 @@ def result():
     global a
     if request.method == 'POST':
         result = request.form
-        functie(result)
+        # functie(result)
         a = result
     return redirect("http://ecnaoptriha.herokuapp.com/result", code=302)
     # return render_template("result.html",result = a)
