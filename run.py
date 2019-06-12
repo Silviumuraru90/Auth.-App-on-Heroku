45c85cb8de9a09ad9fa78cc92db2a353b720b336
Silviu Muraru <silviumuraru90@gmail.com>
2019-06-12 09:39:04 +0300

    Update run.py

--- run.py
+++ run.py
@@ -82,7 +82,7 @@ def result():
 #     # return redirect("http://ecnaoptriha.herokuapp.com/result", code=302)
 #     return render_template("result.html",result = a)
 
-# def cacau():
+# def ceva():
 #     global a
 #     if a:
 #         functie(a)
